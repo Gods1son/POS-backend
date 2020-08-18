fa8adfdd04245630fd2999780b604f9b1705eccb
Gods1son <tolulopeibiyode@gmail.com>
2020-08-17 23:49:15 -0400

    stripe payment added

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var cors = require('cors');
 var dotenv = require('dotenv').config();
 var SuperLogin = require('superlogin');
 var { security } = require('superlogin/config/default.config');
+const Stripe = require('stripe');
+const stripe = Stripe(process.env.stripeKey);
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -41,6 +43,9 @@ var config = {
     loginOnRegistration: true,
     // If set to true, the user will be logged in automatically after resetting the password
     loginOnPasswordReset: true
+  },
+  local: {
+    emailUsername: true
   },
   dbServer: {
     protocol: 'https://',
@@ -176,10 +181,28 @@ app.use('/logoutSession',
     res.send("Welcome here! " + process.env.TESTING);
   });
 
+  app.use('/testQuery',
+  function(req, res) {
+    res.send("Value: " + req.query.value);
+  });
+
   app.use('/checkUsername',
       function(req, res) {
         try{
         superlogin.validateUsername(req.body.username).then(function(data){
+          res.send(data);
+        }).catch(function(err){
+          res.send(err);
+        });
+      }catch(err){
+        console.log(err);
+      }
+  });
+
+  app.use('/checkEmailUsername',
+      function(req, res) {
+        try{
+        superlogin.validateEmailUsername(req.body.email).then(function(data){
           res.send(data);
         }).catch(function(err){
           res.send(err);
@@ -210,6 +233,247 @@ app.use('/logoutSession',
       }).catch(function(err){
         res.send(err);
       });
+    }catch(err){
+      console.log(err);
+    }
+  });
+
+  //stripe integration
+  app.use(
+    '/stripe-webhook',
+    bodyParser.raw({ type: 'application/json' }),
+    async (req, res) => {
+      // Retrieve the event by verifying the signature using the raw body and secret.
+      let event;
+  
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          req.headers['stripe-signature'],
+          process.env.STRIPE_WEBHOOK_SECRET
+        );
+      } catch (err) {
+        console.log(err);
+        return res.sendStatus(400);
+      }
+      // Extract the object from the event.
+      const dataObject = event.data.object;
+
+      // https://stripe.com/docs/billing/webhooks
+      // Remove comment to see the various objects sent for this sample
+      switch (event.type) {
+        case 'invoice.paid':
+          // Used to provision services after the trial has ended.
+          // The status of the invoice will show up as paid. Store the status in your
+          // database to reference when a user accesses your service to avoid hitting rate limits.
+          break;
+        case 'invoice.payment_failed':
+          // If the payment fails or the customer does not have a valid payment method,
+          //  an invoice.payment_failed event is sent, the subscription becomes past_due.
+          // Use this webhook to notify your user that their payment has
+          // failed and to retrieve new card details.
+          break;
+        case 'invoice.finalized':
+          // If you want to manually send out invoices to your customers
+          // or store them locally to reference to avoid hitting Stripe rate limits.
+          break;
+        case 'customer.subscription.deleted':
+          if (event.request != null) {
+            // handle a subscription cancelled by your request
+            // from above.
+          } else {
+            // handle subscription cancelled automatically based
+            // upon your subscription settings.
+          }
+          break;
+        case 'customer.subscription.trial_will_end':
+          if (event.request != null) {
+            // handle a subscription cancelled by your request
+            // from above.
+          } else {
+            // handle subscription cancelled automatically based
+            // upon your subscription settings.
+          }
+          break;
+        default:
+        // Unexpected event type
+      }
+      res.sendStatus(200);
+    }
+  );
+
+  app.use('/createStripeUser',
+  async function(req, res) {
+    try{
+      const customer = await stripe.customers.create({
+        name: req.body.email,
+        email: req.body.email
+      });
+      //console.log(customer);
+      res.send(customer);
+    }catch(err){
+      console.log(err);
+    }
+  });
+
+  app.use('/create-subscription', async (req, res) => {
+    // Attach the payment method to the customer
+    try {
+      await stripe.paymentMethods.attach(req.body.paymentMethodId, {
+        customer: req.body.customerId,
+      });
+    } catch (error) {
+      return res.status('402').send({ error: { message: error.message } });
+    }
+  
+    // Change the default invoice settings on the customer to the new payment method
+    await stripe.customers.update(
+      req.body.customerId,
+      {
+        invoice_settings: {
+          default_payment_method: req.body.paymentMethodId,
+        },
+      }
+    );
+  
+    // Create the subscription
+    const subscription = await stripe.subscriptions.create({
+      customer: req.body.customerId,
+      items: [{ price: process.env.priceId}],
+      expand: ['latest_invoice.payment_intent'],
+    });
+  
+    res.send(subscription);
+  });
+
+  app.use('/retry-invoice', async (req, res) => {
+    // Set the default payment method on the customer
+  
+    try {
+      await stripe.paymentMethods.attach(req.body.paymentMethodId, {
+        customer: req.body.customerId,
+      });
+      await stripe.customers.update(req.body.customerId, {
+        invoice_settings: {
+          default_payment_method: req.body.paymentMethodId,
+        },
+      });
+    } catch (error) {
+      // in case card_decline error
+      return res
+        .status('402')
+        .send({ result: { error: { message: error.message } } });
+    }
+  
+    const invoice = await stripe.invoices.retrieve(req.body.invoiceId, {
+      expand: ['payment_intent'],
+    });
+    res.send(invoice);
+  });
+
+  app.use('/cancel-subscription', async (req, res) => {
+    // Delete the subscription
+    const deletedSubscription = await stripe.subscriptions.del(
+      req.body.subscriptionId
+    );
+    res.send(deletedSubscription);
+  });
+
+  app.use('/check-subscription', async (req, res) => {
+    // Delete the subscription
+    const subscriptions = await stripe.subscriptions.list({
+      customer: req.body.customer,
+      limit:3
+    });
+    
+    var retObj = subscriptions.data.filter(x => x.customer == req.body.customer);
+    res.send(retObj);
+  });
+
+  app.use('/retrieve-customer-payment-method', async (req, res) => {
+    const paymentMethod = await stripe.paymentMethods.retrieve(
+      req.body.paymentMethodId
+    );
+  
+    res.send(paymentMethod);
+  });
+
+  app.use('/getStripeUserByEmail',
+  async function(req, res) {
+    try{
+      const customers = await stripe.customers.list({
+        email: req.body.email,
+        limit: 3,
+      });
+      var retObj = customers.data.filter(x => x.email == req.body.email);
+      res.send(retObj);
+    }catch(err){
+      console.log(err);
+    }
+  });
+
+  app.use('/getStripeUser',
+  async function(req, res) {
+    try{
+      const customer = await stripe.customers.retrieve(
+        'cus_HonXWLstpCth7M'
+      );
+      //console.log(customer);
+      res.send(customer);
+    }catch(err){
+      console.log(err);
+    }
+  });
+
+  app.use('/createStripeProduct',
+  async function(req, res) {
+    try{
+      const product = await stripe.products.create({
+        name: 'Tea Special',
+      });
+      //console.log(customer);
+      res.send("Product " + JSON.stringify(product));
+    }catch(err){
+      console.log(err);
+    }
+  });
+
+  app.use('/createStripePrice',
+  async function(req, res) {
+    try{
+      const price = await stripe.prices.create({
+        unit_amount: 2000,
+        currency: 'cad',
+        product: 'prod_HonkeyuAsQNH5U'
+      });
+      //console.log(customer);
+      res.send("Product " + JSON.stringify(price));
+    }catch(err){
+      console.log(err);
+    }
+  });
+
+  app.use('/createStripeSession',
+  async function(req, res) {
+    try{
+      const sessionStripe = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'T-shirt',
+            },
+            unit_amount: 2000,
+          },
+          quantity: 1,
+        }],
+        mode: 'payment',
+        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        cancel_url: 'https://example.com/cancel',
+      });
+      //console.log(customer);
+      res.send("Session: " + JSON.stringify(sessionStripe));
     }catch(err){
       console.log(err);
     }
